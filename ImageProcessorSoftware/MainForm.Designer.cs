b8d08c94d108cae712f77088861f541e19398272
kharre <matteopecout@free.fr>
2020-10-26 17:09:58 +0100

    Ajoutez des fichiers projet.

---
+++ ImageProcessorSoftware/MainForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ImageProcessorSoftware
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shadesOfGrayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToASCIIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inverseColorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oldStyledToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.filterToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(538, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exportToASCIIToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inverseColorsToolStripMenuItem,
+            this.oldStyledToolStripMenuItem,
+            this.shadesOfGrayToolStripMenuItem,
+            this.sepiaToolStripMenuItem});
+            this.filterToolStripMenuItem.Enabled = false;
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.filterToolStripMenuItem.Text = "Filter";
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainPictureBox.Location = new System.Drawing.Point(12, 27);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(512, 512);
+            this.mainPictureBox.TabIndex = 1;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem});
+            this.editToolStripMenuItem.Enabled = false;
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // shadesOfGrayToolStripMenuItem
+            // 
+            this.shadesOfGrayToolStripMenuItem.Name = "shadesOfGrayToolStripMenuItem";
+            this.shadesOfGrayToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shadesOfGrayToolStripMenuItem.Text = "Shades of gray";
+            this.shadesOfGrayToolStripMenuItem.Click += new System.EventHandler(this.shadesOfGrayToolStripMenuItem_Click);
+            // 
+            // exportToASCIIToolStripMenuItem
+            // 
+            this.exportToASCIIToolStripMenuItem.Enabled = false;
+            this.exportToASCIIToolStripMenuItem.Name = "exportToASCIIToolStripMenuItem";
+            this.exportToASCIIToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToASCIIToolStripMenuItem.Text = "Export to ASCII";
+            this.exportToASCIIToolStripMenuItem.Click += new System.EventHandler(this.exportToASCIIToolStripMenuItem_Click);
+            // 
+            // inverseColorsToolStripMenuItem
+            // 
+            this.inverseColorsToolStripMenuItem.Name = "inverseColorsToolStripMenuItem";
+            this.inverseColorsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inverseColorsToolStripMenuItem.Text = "Inverse colors";
+            this.inverseColorsToolStripMenuItem.Click += new System.EventHandler(this.inverseColorsToolStripMenuItem_Click);
+            // 
+            // oldStyledToolStripMenuItem
+            // 
+            this.oldStyledToolStripMenuItem.Name = "oldStyledToolStripMenuItem";
+            this.oldStyledToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.oldStyledToolStripMenuItem.Text = "Old styled";
+            this.oldStyledToolStripMenuItem.Click += new System.EventHandler(this.oldStyledToolStripMenuItem_Click);
+            // 
+            // sepiaToolStripMenuItem
+            // 
+            this.sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
+            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sepiaToolStripMenuItem.Text = "Sepia";
+            this.sepiaToolStripMenuItem.Click += new System.EventHandler(this.sepiaToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 551);
+            this.Controls.Add(this.mainPictureBox);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Processor Software";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shadesOfGrayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToASCIIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inverseColorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oldStyledToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sepiaToolStripMenuItem;
+    }
+}
+
---
+++ ImageProcessorSoftware/MainForm.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageProcessorSoftware
+{
+    public partial class MainForm : Form
+    {
+        private Bitmap image;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "BMP|*.bmp|GIF|*.gif|JPEG|*.jpeg|PNG|*.png";
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Multiselect = false;
+
+                if (openFileDialog.ShowDialog().Equals(DialogResult.OK))
+                {
+                    try
+                    {
+                        image = new Bitmap(openFileDialog.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"{ex}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    mainPictureBox.Image = image;
+                    saveToolStripMenuItem.Enabled = true;
+                    exportToASCIIToolStripMenuItem.Enabled = true;
+                    editToolStripMenuItem.Enabled = true;
+                    filterToolStripMenuItem.Enabled = true;
+                }
+            }
+        }
+
+        private void exportToASCIIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "TXT|*.txt";
+                saveFileDialog.InitialDirectory = "C:\\";
+
+                if (saveFileDialog.ShowDialog().Equals(DialogResult.OK))
+                {
+                    ExportToASCII(saveFileDialog.FileName);
+                    MessageBox.Show($"The file \"{saveFileDialog.FileName}\" has been exported to ASCII with success.", "Export to ASCII", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "BMP|*.bmp|GIF|*.gif|JPEG|*.jpeg|PNG|*.png";
+                saveFileDialog.InitialDirectory = "C:\\";
+
+                if (saveFileDialog.ShowDialog().Equals(DialogResult.OK))
+                {
+                    mainPictureBox.Image.Save(saveFileDialog.FileName);
+                    MessageBox.Show($"The file \"{saveFileDialog.FileName}\" has been saved with success.", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainPictureBox.Image = image;
+        }
+
+        private void inverseColorsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainPictureBox.Image = InverseColors((Bitmap)mainPictureBox.Image, 0, 0, mainPictureBox.Image.Width, mainPictureBox.Image.Height);
+        }
+
+        private void oldStyledToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainPictureBox.Image = GetOldStyledImage((Bitmap)mainPictureBox.Image, 0, 0, mainPictureBox.Image.Width, mainPictureBox.Image.Height);
+        }
+
+        private void shadesOfGrayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainPictureBox.Image = GetGrayShades((Bitmap)mainPictureBox.Image, 0, 0, mainPictureBox.Image.Width, mainPictureBox.Image.Height);
+        }
+
+        private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainPictureBox.Image = GetSepiaImage((Bitmap)mainPictureBox.Image, 0, 0, mainPictureBox.Image.Width, mainPictureBox.Image.Height);
+        }
+
+        private void ExportToASCII(string fileName)
+        {
+            byte scaleX = 8;
+            byte scaleY = scaleX;
+
+            char[,] ascii = new char[mainPictureBox.Image.Width, mainPictureBox.Image.Height];
+            Bitmap lowRes = new Bitmap(mainPictureBox.Image.Width / scaleX, mainPictureBox.Image.Height / scaleY);
+
+            for (var x = 0; x < mainPictureBox.Image.Width - scaleX; x += scaleX)
+            {
+                for (var y = 0; y < mainPictureBox.Image.Height - scaleY; y += scaleY)
+                {
+                    Color currentPixelColor = ((Bitmap)mainPictureBox.Image).GetPixel(x, y);
+                    lowRes.SetPixel(x / scaleX, y / scaleY, currentPixelColor);
+                }
+            }
+
+            for (var x = 0; x < lowRes.Width; x++)
+            {
+                for (var y = 0; y < lowRes.Height; y++)
+                {
+                    Color currentPixelColor = lowRes.GetPixel(x, y);
+                    var shade = (byte)((currentPixelColor.R + currentPixelColor.G + currentPixelColor.B) / 3);
+                    if ((shade <= 255) && (shade > 192))
+                    {
+                        ascii[x, y] = '.';
+                    }
+                    else if ((shade <= 192) && (shade > 128))
+                    {
+                        ascii[x, y] = 'o';
+                    }
+                    else if ((shade <= 128) && (shade > 64))
+                    {
+                        ascii[x, y] = '0';
+                    }
+                    else if ((shade <= 64) && (shade >= 0))
+                    {
+                        ascii[x, y] = '@';
+                    }
+                }
+            }
+
+            string[] lines = new string[image.Height];
+
+            for (int x = 0; x < lowRes.Height; x++)
+            {
+                string line = "";
+                for (int y = 0; y < lowRes.Width; y++)
+                {
+                    line += ascii[y, x] + "  ";
+                }
+                lines[x] = line;
+            }
+
+            System.IO.File.WriteAllLines($"{fileName}", lines);
+        }
+
+        private Bitmap GetGrayShades(Bitmap source, int sourceX, int sourceY, int sourceWidth, int sourceHeight)
+        {
+            Bitmap destination = new Bitmap(sourceWidth, sourceHeight);
+            for (var i = 0; i < destination.Width; i++)
+            {
+                for (var j = 0; j < destination.Height; j++)
+                {
+                    Color currentPixelColor = source.GetPixel(i + sourceX, j + sourceY);
+                    byte shade = (byte)((currentPixelColor.R + currentPixelColor.G + currentPixelColor.B) / 3);
+                    destination.SetPixel(i, j, Color.FromArgb(shade, shade, shade));
+                }
+            }
+            return destination;
+        }
+
+        private Bitmap GetOldStyledImage(Bitmap source, int sourceX, int sourceY, int sourceWidth, int sourceHeight)
+        {
+            Bitmap destination = new Bitmap(sourceWidth, sourceHeight);
+            for (var i = 0; i < source.Width; i += 2)
+            {
+                for (var j = 0; j < source.Height; j += 2)
+                {
+                    Color currentPixelColor = source.GetPixel(i + sourceX, j + sourceY);
+                    destination.SetPixel(i, j, currentPixelColor);
+                }
+            }
+            return destination;
+        }
+
+        public static Bitmap GetSepiaImage(Bitmap source, int sourceX, int sourceY, int sourceWidth, int sourceHeight)
+        {
+            Bitmap destination = new Bitmap(sourceWidth, sourceHeight);
+            for (var i = 0; i < destination.Width; i++)
+            {
+                for (var j = 0; j < destination.Height; j++)
+                {
+                    Color currentPixelColor = source.GetPixel(i + sourceX, j + sourceY);
+                    byte shade = (byte)((currentPixelColor.R + currentPixelColor.G + currentPixelColor.B) / 3);
+                    if (!(shade > 255 - 48))
+                    {
+                        destination.SetPixel(i, j, Color.FromArgb(shade + 48, shade + 32, shade));
+                    }
+                    else
+                    {
+                        destination.SetPixel(i, j, Color.FromArgb(shade + (255 - shade), shade + (255 - shade), shade));
+                    }
+                }
+            }
+            return destination;
+        }
+
+        private Bitmap InverseColors(Bitmap source, int sourceX, int sourceY, int sourceWidth, int sourceHeight)
+        {
+            Bitmap destination = new Bitmap(source.Width, source.Height);
+            for (var i = 0; i < source.Width; i++)
+            {
+                for (var j = 0; j < source.Height; j++)
+                {
+                    Color currentPixelColor = source.GetPixel(i + sourceX, j + sourceY);
+                    Color newPixelColor = Color.FromArgb(255 - currentPixelColor.R, 255 - currentPixelColor.G, 255 - currentPixelColor.B);
+                    destination.SetPixel(i, j, newPixelColor);
+                }
+            }
+            return destination;
+        }
+    }
+}
